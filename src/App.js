721c296dc238ffe546281ad969c4d2c1070e7815
SebaLapalma <e.sebastian.lapalma@gmail.com>
2023-05-04 14:11:01 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react'
+import Productos from './components/Productos'
+import NavBar from './components/NavBar'
+import Layout from './components/Layout'
+import Title from './components/Title'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    productos: [
+      {
+        name: 'Invictus',
+        price: 6300,
+        img: '/productos/invictus.jpg'
+      },
+      {
+        name: 'Good Girl',
+        price: 6300,
+        img: '/productos/goodgirl.jpg'
+      },
+      {
+        name: 'Bad Boy',
+        price: 6300,
+        img: '/productos/bad boy.jpg'
+      },
+    ],
+    carrito: [],
+    esCarritoVisible: false,
+  }
+
+  agregarAlCarrito = (producto) => {
+    const {carrito} = this.state
+    if (carrito.find( x => x.name === producto.name )){
+      const newCarrito = carrito.map( x => x.name === producto.name
+        ? ({
+          ...x,
+          cantidad: x.cantidad + 1
+        })
+        : x)
+      return this.setState({ carrito: newCarrito })
+    }
+
+    return this.setState({
+      carrito: this.state.carrito.concat({
+        ...producto, 
+        cantidad: 1,
+      })
+    })
+  }
+
+  mostrarCarrito = () => {
+    if(!this.state.carrito.length){
+      return
+    }
+    this.setState({esCarritoVisible: !this.state.esCarritoVisible})
+  }
+
+  render() {
+    const {esCarritoVisible} = this.state
+    return (
+      <div>
+        <NavBar
+          carrito={this.state.carrito}
+          esCarritoVisible={esCarritoVisible}
+          mostrarCarrito={this.mostrarCarrito}
+        />
+        <Layout>
+          <Title />
+          <Productos
+            agregarAlCarrito = {this.agregarAlCarrito}
+            productos = {this.state.productos}
+          />
+        </Layout>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import Logo from './Logo'
+import Carrito from './Carrito'
+
+const styles = {
+    navbar: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        height: '100px',
+        justifyContent: 'space-between',
+        position: 'relative',
+        padding: '0 50px',
+        boxShadow: '0 2px 3px rgb(0, 0, 0, 0.1)'
+    }
+}
+
+class NavBar extends Component {
+  render() {
+    const {esCarritoVisible, mostrarCarrito} = this.props
+    const {carrito} = this.props
+    return (
+        <nav style={styles.navbar}>
+            <Logo />
+            <Carrito 
+             carrito={carrito}
+             esCarritoVisible={esCarritoVisible}
+             mostrarCarrito={mostrarCarrito}
+            />
+        </nav>
+    )
+  }
+}
+
+export default NavBar
